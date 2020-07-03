da6659116686091ca3376f5bd67637b41a4dd3f8
xiaoxian <591972700@qq.com>
2020-07-03 11:41:11 +0800

    xadmin配置

--- TravelWeb/TravelWeb/settings.py
+++ TravelWeb/TravelWeb/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+import sys
+sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #第三方
+    'xadmin',
+    'crispy_forms',
+    'reversion',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'TravelWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TravelWebsite',                # 数据库名称， 要在MySQL中创建一个名为TravelWebsite的数据库
+        'USER': "root",
+        'PASSWORD': "mysql",
+        'HOST': "192.168.1.102",
+
     }
 }
 
@@ -103,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'      # 语言
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'    # 时区
 
 USE_I18N = True
 
